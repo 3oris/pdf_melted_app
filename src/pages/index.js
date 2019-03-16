285be6878b190101244e482d57e32931aceae1c5
Boris Erdmann <boris.erdmann@gmail.com>
2019-03-16 13:49:44 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () =>
+
+  <div id="helloWorld">
+    <h1>Hello world!</h1>
+    <p>
+      Example to generate and build and include Pdf documents. <br/>
+      <a href="./pdf_documents/pdf_sample.pdf">View sample PDF</a>
+    </p>
+  </div>
