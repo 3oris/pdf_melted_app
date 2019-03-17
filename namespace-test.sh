16bbd464c9be2bb2a039864d396f33dbae99f6c7
Boris Erdmann <boris.erdmann@gmail.com>
2019-03-17 10:44:13 +0100

    buildspec

---
+++ namespace-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VALUE=$(cat /boot/config-$(uname -r) | grep CONFIG_USER_NS)
+
+if [[ -z "$VALUE" ]]
+then
+  echo 'You do not have namespacing in the kernel. You will need to enable the SUID sandbox or upgrade your kernel.';
+  exit 1
+fi
+
+USER_NS_AVAILABLE="${VALUE: -1}"
+
+if [[ "$USER_NS_AVAILABLE" -eq "y" ]]
+then
+  echo 'You have user namespacing in the kernel. You should be good to go.';
+  exit 0
+else
+  echo 'You do not have namespacing in the kernel. You will need to enable the SUID sandbox or upgrade your kernel.';
+  exit 1
+fi
